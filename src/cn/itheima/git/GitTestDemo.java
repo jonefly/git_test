034eccff93b01c4a5bffcad1fa96b49bed5d5ef7
renshaofeng <sfrenc@isoftstone.com>
2018-11-05 16:33:54 +0800

    添加getAddress方法

--- src/cn/itheima/git/GitTestDemo.java
+++ src/cn/itheima/git/GitTestDemo.java
@@ -1,5 +1,7 @@
 package cn.itheima.git;
 
+import java.util.UUID;
+
 public class GitTestDemo {
     public static void main(String[] args) {
         System.out.println("git测试代码1");
@@ -7,6 +9,12 @@ public class GitTestDemo {
         System.out.println("git测试代码3");
         System.out.println("git测试代码4");
         System.out.println("git测试代码5");
+        getAddress();
+        System.out.println(getAddress());
+    }
+
+    private static  String   getAddress(){
+        return UUID.randomUUID().toString();
     }
 
 }
