e05bd93a753fa55130537a0397b11e512757f6f0
renshaofeng <sfrenc@isoftstone.com>
2018-11-05 16:27:35 +0800

    git测试代码

---
+++ src/cn/itheima/git/GitTestDemo.java
@@ -0,0 +1,12 @@
+package cn.itheima.git;
+
+public class GitTestDemo {
+    public static void main(String[] args) {
+        System.out.println("git测试代码1");
+        System.out.println("git测试代码2");
+        System.out.println("git测试代码3");
+        System.out.println("git测试代码4");
+        System.out.println("git测试代码5");
+    }
+
+}
